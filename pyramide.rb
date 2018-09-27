7ce2c7fed28f882c7f76c5371d6d4f468b044156
SAMTHP <43207100+SAMTHP@users.noreply.github.com>
2018-09-27 18:17:46 +0200

    Ajout d'une condition

--- pyramide.rb
+++ pyramide.rb
@@ -3,10 +3,15 @@ etage = gets.chomp.to_i
 puts "Voici la pyramide :"
 
 bloc = "#"
-etage.times do |i|
-	space =" "
-	espace = space*etage
-	puts espace + bloc
-	bloc = bloc + "#"
-	etage = etage - 1
+
+if etage <= 25 
+	etage.times do |i|
+		space =" "
+		espace = space*etage
+		puts espace + bloc
+		bloc = bloc + "#"
+		etage = etage - 1
+	end
+else 
+	puts "Veuillez ne pas dépasser 25 étages."
 end
