b47b68989f919b5d72b62e88a2b208d48aaad6a1
SAMTHP <43207100+SAMTHP@users.noreply.github.com>
2018-09-28 16:56:29 +0200

    ajout méthode .even sur exo_19.rb

---
+++ exo_19_pair.rb
@@ -0,0 +1,23 @@
+puts "Création de vos emails :"
+
+# Demande du prénom et du nom
+puts "Quel est votre prénom ?"
+prenom = gets.chomp
+puts "Quel est votre nom ?"
+nom = gets.chomp
+
+# Création de l'email
+num = 0 # Identifian initial
+
+# Détermination de la boucle
+while num <= 50 do
+	#pair = 2 # Déclaration et affection de la variable pair
+	email = "#{prenom}.#{nom}.0#{num}@email.com"
+	tab = []
+	tab << email
+		if num.even?
+		puts tab
+		end
+	num = num+1
+	#pair = pair+2 	
+end
